5ed9539d9853e877526abb59cd60e3fb2f2ba091
root <tanakamasayuki@gmail.com>
2024-06-28 09:41:12 +0900

    update

--- EVT/EXAM/USART/USART_DMA/User/ch32x035_conf.h
+++ EVT/EXAM/USART/USART_DMA/User/ch32x035_conf.h
@@ -1,39 +1,20 @@
 /********************************** (C) COPYRIGHT *******************************
- * File Name          : ch32x035_conf.h
+ * File Name          : ch32x035_it.h
  * Author             : WCH
  * Version            : V1.0.0
  * Date               : 2023/04/06
- * Description        : Library configuration file.
+ * Description        : This file contains the headers of the interrupt handlers.
 *********************************************************************************
 * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
 * Attention: This software (modified or not) and binary are used for 
 * microcontroller manufactured by Nanjing Qinheng Microelectronics.
 *******************************************************************************/
-#ifndef __CH32X035_CONF_H
-#define __CH32X035_CONF_H
+#ifndef __CH32X035_IT_H
+#define __CH32X035_IT_H
 
-#include "ch32x035_adc.h"
-#include "ch32x035_awu.h"
-#include "ch32x035_dbgmcu.h"
-#include "ch32x035_dma.h"
-#include "ch32x035_exti.h"
-#include "ch32x035_flash.h"
-#include "ch32x035_gpio.h"
-#include "ch32x035_i2c.h"
-#include "ch32x035_iwdg.h"
-#include "ch32x035_pwr.h"
-#include "ch32x035_rcc.h"
-#include "ch32x035_spi.h"
-#include "ch32x035_tim.h"
-#include "ch32x035_usart.h"
-#include "ch32x035_wwdg.h"
-#include "ch32x035_it.h"
-#include "ch32x035_misc.h"
+#include "debug.h"
 
 
 #endif
 
 
-	
-	
-	
