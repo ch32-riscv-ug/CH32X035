bcc9b8b17c58ebe0d91c09b961a8ee0698cb63be
root <tanakamasayuki@gmail.com>
2024-07-05 10:47:09 +0900

    update

--- update.sh
+++ update.sh
@@ -7,20 +7,20 @@ git pull
 # https://www.wch-ic.com/products/CH32X035.html
 cd datasheet_en
 # https://www.wch-ic.com/downloads/CH32X035DS0_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/381.html -O CH32X035DS0.PDF
+curl -z CH32X035DS0.PDF -o CH32X035DS0.PDF https://www.wch-ic.com/downloads/file/381.html
 # https://www.wch-ic.com/downloads/CH32X035RM_PDF.html
-wget --continue https://www.wch-ic.com/downloads/file/382.html -O CH32X035RM.PDF
+curl -z CH32X035RM.PDF -o CH32X035RM.PDF https://www.wch-ic.com/downloads/file/382.html
 cd ..
 
 cd datasheet_zh
 # https://www.wch.cn/downloads/CH32X035DS0_PDF.html
-wget --continue https://www.wch.cn/downloads/file/443.html -O CH32X035DS0.PDF
+curl -z CH32X035DS0.PDF -o CH32X035DS0.PDF https://www.wch.cn/downloads/file/443.html
 # https://www.wch.cn/downloads/CH32X035RM_PDF.html
-wget --continue https://www.wch.cn/downloads/file/445.html -O CH32X035RM.PDF
+curl -z CH32X035RM.PDF -o CH32X035RM.PDF https://www.wch.cn/downloads/file/445.html
 cd ..
 
 # https://www.wch.cn/downloads/CH32X035EVT_ZIP.html
-wget --continue https://www.wch.cn/downloads/file/444.html -O CH32X035EVT.ZIP
+curl -z CH32X035EVT.ZIP -o CH32X035EVT.ZIP https://www.wch.cn/downloads/file/444.html
 rm -rfv EVT
 unzip -O GB2312 *.ZIP
 
